44cfad37d206be0c1d69cdacf4d6108eb3aa085e
Josh Gilkey <joshtgilkey@gmail.com>
2023-07-24 11:32:25 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DND_Character_Sheet_Webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
